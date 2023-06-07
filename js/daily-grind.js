07890d46ea0b72a08cd68c6b8dd393ef8c7ecad0
Kaleb Castle <kalebm.castle@gmail.com>
2023-06-06 21:35:16 -0700

    updated index

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -15,6 +15,15 @@ let today = myDate.getDay();
 
 let coffee = "";
 
+//use location object to access querystring (address bar)
+const queryString = window.location.search;    
+//output to console    
+console.log(queryString);
+    
+//separate query string parameters
+const urlParams = new URLSearchParams(queryString);
+
+
 function coffeeTemplate(coffee){
 
 return `
@@ -26,12 +35,27 @@ return `
 
 //today = 3;
 
-
+if(urlParams.has("day")){//from querystring
+    today = urlParams.get("day");
+ }
+ today = parseInt(today);
 
 switch(today){
-    /*case 0:
+
+    case 0:
         today = "Sunday";
-    break;*/ 
+
+        coffee = {
+            name: "Drip",
+            pic: "images/drip.jpg",
+            alt: "A picutre of a drip coffee",
+            color: "grey",
+            day: "Sunday",
+            desc: `I love me some dripp coffee!`
+        };
+
+
+    break; 
     
     case 1:
         today = "Monday";
@@ -41,7 +65,7 @@ switch(today){
             pic: "images/bubble-tea.jpg",
             alt: "A picutre of a bubble tea",
             color: "pink",
-            day: "Wednesday",
+            day: "Monday",
             desc: `I love me some bubble tea!`
         };
 
@@ -50,28 +74,85 @@ switch(today){
     
     case 2:
         today = "Tuesday";
+
+        coffee = {
+            name: "Mocha",
+            pic: "images/Mocha.jpg",
+            alt: "A picutre of a Mocha coffee",
+            color: "BurlyWood",
+            day: "Tuesday",
+            desc: `I love me some Mocha`
+        };
+
+
     break;
 
     case 3:
         today = "Wednesday";
+
+        coffee = {
+            name: "Pumpkin Spice Latte",
+            pic: "images/pumpkin-spice-latte.jpg",
+            alt: "A picutre of a pumpkin spice latte ",
+            color: "Coral",
+            day: "Wednesday",
+            desc: `I love me some pumpkin spice!`
+        };
+
+
     break;
     
-   /* case 4:
+    case 4:
         today = "Thursday";
+
+        coffee = {
+            name: "Cold brew",
+            pic: "images/cold-brew.jpg",
+            alt: "A picutre of a cold brew coffee",
+            color: "lightblue",
+            day: "Thursday",
+            desc: `I love me some cold brew!`
+        };
+
+
     break;
     
     case 5:
         today = "Friday";
+
+        coffee = {
+            name: "Frappaccino",
+            pic: "images/frappaccino.jpg",
+            alt: "A picutre of a frappaccino",
+            color: "lightgreen",
+            day: "Friday",
+            desc: `I love me some frappaccino!`
+        };
+
+
     break;
 
     case 6:
         today = "Saturday";
-    break;*/
+
+        coffee = {
+            name: "Caramel latte",
+            pic: "images/caramel-latte.jpg",
+            alt: "A picutre of a caramel latte ",
+            color: "caramel",
+            day: "Wednesday",
+            desc: `I love me some caramel latte !`
+        };
+
+
+    break;
     default:
         today = "Day is Unknown";
 }
 
  
 document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+document.querySelector("html").style.backgroundColor = coffee.color
 
 console.log(coffee);
